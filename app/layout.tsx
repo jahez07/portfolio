de2cffe6e1e96b95d7997ebf301594a522cb8853
jahez07 <61622091+jahez07@users.noreply.github.com>
2025-09-08 22:55:11 +0400

    Google Analytics Integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -65,6 +66,7 @@ export default function RootLayout({
           <SpeedInsights />
         </main>
       </body>
+      <GoogleAnalytics gaId='G-RN8Z8BFVM8'/>
     </html>
   )
 }
