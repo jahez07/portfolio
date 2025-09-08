e27ce49c7e5016b939b0b82002dc70087b5938ce
jahez07 <61622091+jahez07@users.noreply.github.com>
2025-09-08 22:21:26 +0400

    upper case to lower case

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`as a passionate ai engineer i thrive on the thrill of creating innovative applications that push the boundaries of technology. 
-        My love for community collaboration and teamwork fuels my drive to contribute to projects that make a difference. 
-        Beyond the world of code and algorithms, I am an artist at heart, infusing creativity into every aspect of my work. 
-        Whether I’m developing cutting-edge AI solutions or crafting a piece of art, my goal is always to blend technical prowess with imaginative flair. 
-        Let's build something amazing together!`}
+        my love for community collaboration and teamwork fuels my drive to contribute to projects that make a difference. 
+        beyond the world of code and algorithms, i am an artist at heart, infusing creativity into every aspect of my work. 
+        Whether i’m developing cutting-edge ai solutions or crafting a piece of art, my goal is always to blend technical prowess with imaginative flair. 
+        let's build something amazing together!`}
       </p>
       <div className="my-8">
         <BlogPosts />
